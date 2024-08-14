e0bfd87582b37be8ec5936b489ba9729767cdce5
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-08-14 23:33:29 +0330

    Initial Commit

---
+++ UIKitPro10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPro10
+//
+//  Created by Sothesom on 24/05/1403.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
