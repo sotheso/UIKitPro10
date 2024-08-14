8d5a9f3c0719214c46e7b3dbdfd7482c5f3dbd4f
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-08-14 23:59:36 +0330

    Reading and writing basics: UserDefaults

--- UIKitPro10/ViewController.swift
+++ UIKitPro10/ViewController.swift
@@ -11,7 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "FaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set(Data(), forKey: "LastRun")
+        
+        let arry = ["Mohammad", "Karimi"]
+        defaults.set(arry, forKey: "SaveArray")
+        
+        let dict = ["Name": "Sothesom", "Country": "IRAN"]
+        defaults.set(dict, forKey: "SaveDic")
+        
+        let saveInteger = defaults.integer(forKey: "Age")
+        let saveBoolean = defaults.bool(forKey: "FaceID")
+        
+        let saveArray = defaults.array(forKey: "SaveArray") as? [String] ?? [String]()
+        let saveDic = defaults.dictionary(forKey: "SaveDic") as? [String: String] ?? [String : String]()
     }
 
 
